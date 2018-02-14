07243b7d87223b63497ec89539754c3aeb8b2ee6
Carlo Vallati <warner83@gmail.com>
2018-02-14 09:12:55 +0100

    Added simple ecoap external example

--- contiki/ecoap_external/global_cp_simple.py
+++ contiki/ecoap_external/global_cp_simple.py
@@ -10,9 +10,10 @@ Usage:
 Options:
    --logfile name      Name of the logfile
    --config configFile Config file path
+   --nodes nodesFile   Config file with node inf   --nodes nodesFile   Config file with node infoo
 
 Example:
-   python global_cp.py --config config/localhost/global_cp_config.yaml
+   python global_cp.py --config config/localhost/global_cp_config.yaml --nodes config/localhost/nodes.yaml
    
 Other options:
    -h, --help          show this help message and exit
@@ -25,6 +26,10 @@ import wishful_controller
 import yaml
 import gevent
 import subprocess
+from contiki.contiki_helpers.global_node_manager import *
+from contiki.contiki_helpers.app_manager import *
+import wishful_upis as upis
+from local_cp import my_local_control_program
 
 __author__ = "Peter Ruckebusch"
 __copyright__ = "Copyright (c) 2016, IMEC"
@@ -32,6 +37,10 @@ __version__ = "0.1.0"
 __email__ = "peter.ruckebusch@intec.ugent.be"
 
 log = logging.getLogger('contiki_global_control_program')
+
+nodes = []
+
+
 
 
 if __name__ == "__main__":
@@ -59,14 +68,6 @@ if __name__ == "__main__":
         logfile = args['--logfile']
     logging.basicConfig(filename=logfile, level=log_level, format='%(asctime)s - %(name)s.%(funcName)s() - %(levelname)s - %(message)s')
 
-    controller = wishful_controller.Controller()
-    nodes = []
-
-    @controller.new_node_callback()
-    def new_node(node):
-        nodes.append(node)
-        print("New node appeared:")
-        print(node)
 
     try:
         config_file_path = args['--config']
@@ -74,8 +75,18 @@ if __name__ == "__main__":
         with open(config_file_path, 'r') as f:
             config = yaml.load(f)
 
+        controller = wishful_controller.Controller()
+
         controller.load_config(config)
         controller.start()
+
+        @controller.new_node_callback()
+        def new_node(node):
+           nodes.append(node)
+
+           print("New node appeared :")
+           print(node)
+        
 
         first_time = True
         coap_server_initialized = False
@@ -124,21 +135,24 @@ if __name__ == "__main__":
         ]
 
         net_event_list = [
-            "app_rx_event"
+            "app_rx_event",
+            "app_tx_timeout"
         ]
 
         while True:
+
             gevent.sleep(5)
             print("\n")
             print("Connected nodes", [str(node.name) for node in nodes])
-            if nodes:
+
+            if len(nodes) > 0:
                 if first_time:
+
+                    ret = controller.blocking(True).node(nodes[0]).net.iface("lowpan0").rpl_set_border_router([0xfd, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
 
                     if int(subprocess.check_output("sudo ip -6 addr add fd00::1/64 dev tun1 2> /dev/null; echo $?", shell=True, universal_newlines=True).strip()) > 0:
                         subprocess.check_output("sudo ip -6 addr add fd00::1/64 dev tun1", shell=True, universal_newlines=True).strip()
 
-                    ret = controller.blocking(True).node(nodes[0]).net.iface("lowpan0").rpl_set_border_router([0xfd, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
-                    print(ret)
                     if int(subprocess.check_output("sudo ip6tables -C INPUT -d fd00::/64 -j ACCEPT 2> /dev/null; echo $?", shell=True, universal_newlines=True).strip()) > 0:
                         subprocess.check_output("sudo ip6tables -I INPUT 1 -d fd00::/64 -j ACCEPT", shell=True, universal_newlines=True).strip()
 
@@ -149,17 +163,18 @@ if __name__ == "__main__":
                     for rp in radio_platforms:
                         ret = controller.blocking(True).node(nodes[0]).net.iface(rp).set_parameters_net({'rpl_objective_function': 1})
                         print(ret)
- 
 
                 else:
                     if coap_server_initialized is False:
-                        #controller.blocking(True).node(nodes[0]).net.create_packetflow_sink(port=5683)
+                        controller.blocking(True).node(nodes[0]).net.create_packetflow_sink(port=5683)
                         coap_server_initialized = True
+
                     for node in nodes:
                         
+
                         ret = controller.blocking(True).node(node).net.iface("lowpan1").set_parameters_net({'app_activate': 2})
                         ret = controller.blocking(True).node(node).net.iface("lowpan2").set_parameters_net({'app_activate': 2})
-                        continue
+
                         for param in radio_param_list:
                             for radio_platform in radio_platforms:
                                 val = controller.blocking(True).node(node).radio.iface(radio_platform).get_parameters([param])
