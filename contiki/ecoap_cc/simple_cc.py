5519af24a4da4e55688936bf81e6865e6052eb09
Carlo Vallati <warner83@gmail.com>
2018-02-26 09:40:15 +0100

    Fixed an issue with Simple CC

--- contiki/ecoap_cc/simple_cc.py
+++ contiki/ecoap_cc/simple_cc.py
@@ -14,10 +14,11 @@ class SimpleCC(BaseCC):
     def tx_success(self, info):
         print(str(info))
         rtt = int(info[2])
+        node_id = int(info[0])
 
         rto = (rtt + randint(0,rtt) , rtt * 2 + randint(0,rtt*2) , rtt * 3 + randint(0,rtt * 3),  rtt * 4 + randint(0,rtt * 4))
 
-        err = self.app_manager.update_async_configuration({"coap_rto": rto})
+        err = self.app_manager.update_async_configuration({"coap_rto": rto}, [node_id])
 
     def tx_failed(self, info):
         pass
